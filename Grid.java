b0d098d73d31e0f8e8dd0d55925871c36770dbfc
sarwin_ <sarwinpersonal2002@gmail.com>
2023-01-04 21:04:23 +0400

    basic plot

--- Grid.java
+++ Grid.java
@@ -7,23 +7,24 @@ import javax.swing.JPanel;
 
 public class Grid extends JPanel{
 
+	private int x, y;
 	private Graphics2D g2D;
 	
 	public Grid() {
 		this.setPreferredSize(new Dimension(500, 500));
-		System.out.println("1");
 	}
 	
 	public void paint(Graphics g) {
 		g2D = (Graphics2D) g;
-		System.out.println("2");
+		
+		g2D.drawRect(x, y, 5, 5);
 	}
 	
-	public void plotPoint(int x, int y) {
-		while (g2D == null) {
-			System.out.println("xxx");
-		}
-		g2D.drawRect(x, y, 5, 5);
+	public void plotPoint(int _x, int _y) {
+		x = _x;
+		y = _y;
+		
+		repaint();
 	}
 	
 }
--- Panel.java
+++ Panel.java
@@ -1,16 +1,32 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class Panel extends JFrame{
+public class Panel extends JFrame implements ActionListener{
+	
+	Random random;
 	
 	Grid grid;
+	JButton button;
 	
 	Panel() {		
 		
+		random = new Random();
+		
 		grid = new Grid();
+		
+		button = new JButton();
+		button.setBounds(200, 100, 100, 50);
+		button.setText("REFRESH");
+		button.addActionListener(this);
 		
 		this.setTitle("MY PANEL");
 		this.setSize(500, 500);
 
+		this.add(button);
 		this.add(grid);
 		this.pack();
 		
@@ -24,5 +40,12 @@ public class Panel extends JFrame{
 		
 		grid.plotPoint(100, 100);
 		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == button) {
+			grid.plotPoint(random.nextInt(5, 500), random.nextInt(5, 500));			
+		}
 	}
 }
