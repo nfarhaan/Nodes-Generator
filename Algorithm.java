c6700bed1501c3d2a96c2e1fa4f3e3a5a6966a62
sarwin_ <sarwinpersonal2002@gmail.com>
2023-01-21 13:46:20 +0400

    i hope it works  :cry:

--- Algorithm.java
+++ Algorithm.java
@@ -11,214 +11,216 @@ public class Algorithm {
 	static Graph graph;
 	public static int[] shortestPath;
 	static double shortestDistance = 99999999;
-	
+
 	static int[] indexPool;
-	static boolean hasSkiped = false; 
-	
+	static boolean hasSkiped = false;
+
+	static boolean isFinished = false;
+
 	static void swap(int[] indexPool, int i, int j) {
-    	int t = indexPool[i];
-        indexPool[i] = indexPool[j];
-        indexPool[j] = t;
-    }
+		int t = indexPool[i];
+		indexPool[i] = indexPool[j];
+		indexPool[j] = t;
+	}
 
-    static void reverse(int indexPool[], int l, int h) {
-        while (l < h) {
-            swap(indexPool, l, h);
-            l++;
-            h--;
-        }
-    }
+	static void reverse(int indexPool[], int l, int h) {
+		while (l < h) {
+			swap(indexPool, l, h);
+			l++;
+			h--;
+		}
+	}
 
-    static int findCeil(int indexPool[], int first, int l, int h) {
-        int ceilIndex = l;
+	static int findCeil(int indexPool[], int first, int l, int h) {
+		int ceilIndex = l;
 
-        for (int i = l + 1; i <= h; i++)
-            if (indexPool[i] > first && indexPool[i] < indexPool[ceilIndex])
-                ceilIndex = i;
+		for (int i = l + 1; i <= h; i++)
+			if (indexPool[i] > first && indexPool[i] < indexPool[ceilIndex])
+				ceilIndex = i;
 
-        return ceilIndex;
-    }
-    
-    static double calculateEulerDistance(int x1, int y1, int x2, int y2) {
-    	return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-    }
-    
-    static double getPathDistance(int[] order) {
-    	double d = 0;
-    	
-    	ArrayList<Integer> green = new ArrayList<Integer>();
-    	ArrayList<Integer> remaining = new ArrayList<Integer>();
-    	
-    	green.clear();
-    	remaining.clear();
-    	
-    	int toIterate = -1;
-    	
-    	green.add(order[0]);
-    	
-    	for (int i = 0; i < order.length - 1; i++) {
-    		
-    		//System.out.print(order[i]);
-    		//green.add(order[i + 1]);
-    		d += calculateEulerDistance(graph.nodes.get(order[i]).posX, graph.nodes.get(order[i]).posY, graph.nodes.get(order[i + 1]).posX, graph.nodes.get(order[i + 1]).posY);
-    		if(d > shortestDistance) {
-    			//System.out.println(order[i + 1]);
-    			toIterate = order[i];
-    			
-    			for (int j = i + 1; j < order.length; j++) {
-    				remaining.add(order[j]);
-    			}
-    			
-    			remaining.add(toIterate);
-    			
-    			if(remaining.size() > 2) {
-    				hasSkiped = true;
-    								
-    				toIterate++;
-    				while (green.contains(toIterate)) {
-    					toIterate++;
-    				}
-    				
-    				int mainCount = 0;
-    				if(toIterate > order.length - 1) {
-    					
-    					int g = -1;
-    					while (true) {
-    						//need to check if we are changing the 1st index and if yes, check if it is not the max. if max then end.
-    						//if(gIndex == 0)
-    						if(green.size() == 1 && mainCount >= 1) {
-    							if(green.get(0) == order.length - 1) {
-    								System.out.println("all done");
-    								return 9999999;
-    								//return someting to end the thing
-    							}
-    						}
-    						
-    						mainCount++;
-    						
-    						g = green.get(green.size() - 1);
-    						
-    						remaining.add(g);
-    						green.remove(Integer.valueOf(g));
-    						
-    						int gIncremented = g + 1;
-    						
-    						boolean found = true;
-    						while (!remaining.contains(gIncremented)) {
-    							gIncremented++;
-    							if(gIncremented > order.length - 1) {
-    								// means that this current element cannot be incremented
-    								// add the value to remaining and continue searching with the previous element
-    								found = false;
-    								break;
-    							} 
-    						}
+		return ceilIndex;
+	}
 
-    						if(found)  {
-    							toIterate = gIncremented;
-    							break;
-    						}
-    					}
-    					//green.set(green.size() - 1, )
-    					// to swap place of latest green value and add the previous value in remaing
-    				}
-    				
-    				//sort the remaning array list. // to do!!
-    				// rearranging the array [green, toIterate, remaining]
-    				
-    				//green.remove(green.size() - 1);
-    				for(int h = 0; h < green.size(); h++) {    				
-    					indexPool[h] = green.get(h);
-    				}
-    				
-    				indexPool[green.size()] = toIterate;
-    				
-    				// to re order remaining
-    				
-    				remaining.remove(Integer.valueOf(toIterate));
-    				Collections.sort(remaining);
-    				
-    				System.out.println(green.toString());
-    				System.out.println(remaining.toString());
-    				System.out.println(toIterate);
-    				
-    				for (int z = 0; z < remaining.size(); z++) {
-    					indexPool[green.size() + 1 + z] = remaining.get(z);
-    				}
-    			}
-    			
-    			return d; // #1 optimization
-    		}
-    		else {
+	static double calculateEulerDistance(int x1, int y1, int x2, int y2) {
+		return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+	}
+
+	static double getPathDistance(int[] order) {
+		double d = 0;
+
+		ArrayList<Integer> green = new ArrayList<Integer>();
+		ArrayList<Integer> remaining = new ArrayList<Integer>();
+
+		green.clear();
+		remaining.clear();
+
+		int toIterate = -1;
+
+		green.add(order[0]);
+
+		for (int i = 0; i < order.length - 1; i++) {
+
+			// System.out.print(order[i]);
+			// green.add(order[i + 1]);
+			d += calculateEulerDistance(graph.nodes.get(order[i]).posX, graph.nodes.get(order[i]).posY,
+					graph.nodes.get(order[i + 1]).posX, graph.nodes.get(order[i + 1]).posY);
+			if (d > shortestDistance) {
+				// System.out.println(order[i + 1]);
+				toIterate = order[i + 1];
+
+				for (int j = i + 2; j < order.length; j++) {
+					remaining.add(order[j]);
+				}
+
+				remaining.add(toIterate);
+
+				if (remaining.size() > 2) {
+					hasSkiped = true;
+
+					toIterate++;
+					while (green.contains(toIterate)) {
+						toIterate++;
+					}
+
+					int mainCount = 0;
+					if (toIterate > order.length - 1) {
+
+						int g = -1;
+						while (true) {
+							// need to check if we are changing the 1st index and if yes, check if it is not
+							// the max. if max then end.
+							// if(gIndex == 0)
+							if (green.size() == 1 && mainCount >= 1) {
+								if (green.get(0) == order.length - 1) {
+									System.out.println("all done");
+
+									isFinished = true;
+									return 9999999;
+									// return someting to end the thing
+								}
+							}
+
+							mainCount++;
+
+							g = green.get(green.size() - 1);
+
+							remaining.add(g);
+							green.remove(Integer.valueOf(g));
+
+							int gIncremented = g + 1;
+
+							boolean found = true;
+							while (!remaining.contains(gIncremented)) {
+								gIncremented++;
+								if (gIncremented > order.length - 1) {
+									// means that this current element cannot be incremented
+									// add the value to remaining and continue searching with the previous element
+									found = false;
+									break;
+								}
+							}
+
+							if (found) {
+								toIterate = gIncremented;
+								break;
+							}
+						}
+						// green.set(green.size() - 1, )
+						// to swap place of latest green value and add the previous value in remaing
+					}
+
+					// sort the remaning array list. // to do!!
+					// rearranging the array [green, toIterate, remaining]
+
+					// green.remove(green.size() - 1);
+					for (int h = 0; h < green.size(); h++) {
+						indexPool[h] = green.get(h);
+					}
+
+					indexPool[green.size()] = toIterate;
+
+					// to re order remaining
+
+					remaining.remove(Integer.valueOf(toIterate));
+					Collections.sort(remaining);
+
+					// System.out.println(green.toString());
+					// System.out.println(remaining.toString());
+					// System.out.println(toIterate);
+
+					for (int z = 0; z < remaining.size(); z++) {
+						indexPool[green.size() + 1 + z] = remaining.get(z);
+					}
+				}
+
+				return d; // #1 optimization
+			} else {
 				green.add(order[i + 1]);
 			}
-    	}
-    	//System.out.println(order[order.length - 1]);
-    	return d;
-    }
-    
-    static void permute(Graph graph) {
-    	shortestDistance = 99999999;
-    	
-    	long startTime = System.currentTimeMillis();
-    	
-    	Algorithm.graph = graph;
-    	
-    	indexPool = new int[graph.nodes.size()];
-        Arrays.setAll(indexPool, i -> i);
-        
-        shortestPath = indexPool;
-        double currentDistance = 0;
-    	
-        int size = indexPool.length;
-
-        boolean isFinished = false;
-        while (!isFinished) {
-        	
-        	hasSkiped = false;
-        	currentDistance = getPathDistance(indexPool);
-        	
-        	for (int i : indexPool) {
-        		System.out.print(i);
-        	}
-        	System.out.println(" - " + currentDistance);
-        	System.out.print("\n");
-        	
-        	if(!hasSkiped) {
-        		if(shortestDistance > currentDistance) {
-        			shortestDistance = currentDistance;
-        			shortestPath = indexPool.clone();
-        		}
-        		
-        		
-        		int i;
-        		for (i = size - 2; i >= 0; --i)
-        			if (indexPool[i] < indexPool[i + 1])
-        				break;
-        		
-        		if (i == -1)
-        			isFinished = true;
-        		else {
-        			int ceilIndex = findCeil(indexPool, indexPool[i], i + 1,
-        					size - 1);
-        			
-        			swap(indexPool, i, ceilIndex);
-        			
-        			reverse(indexPool, i + 1, size - 1);
-        		}
-        	}
-        }
-       
-        String s = "";
-        for (int k : shortestPath) {
-    		s += (char)(k + 65);
 		}
-        System.out.println("shortest path: " + s + " with a distance of " + shortestDistance);
-        
-        
-        long endTime = System.currentTimeMillis();
-        long totalTime = endTime - startTime;
-        System.out.println("The time taken to run the program is " + totalTime / 1000.0 + " seconds");
-    }
-}
+		// System.out.println(order[order.length - 1]);
+		return d;
+	}
 
+	static void permute(Graph graph) {
+		shortestDistance = 99999999;
+
+		long startTime = System.currentTimeMillis();
+
+		Algorithm.graph = graph;
+
+		indexPool = new int[graph.nodes.size()];
+		Arrays.setAll(indexPool, i -> i);
+
+		shortestPath = indexPool;
+		double currentDistance = 0;
+
+		int size = indexPool.length;
+
+		isFinished = false;
+		while (!isFinished) {
+
+			hasSkiped = false;
+			currentDistance = getPathDistance(indexPool);
+
+			// for (int i : indexPool) {
+			// System.out.print(i);
+			// }
+			// System.out.println(" - " + currentDistance);
+			// System.out.print("\n");
+
+			if (!hasSkiped) {
+				if (shortestDistance > currentDistance) {
+					shortestDistance = currentDistance;
+					shortestPath = indexPool.clone();
+				}
+
+				int i;
+				for (i = size - 2; i >= 0; --i)
+					if (indexPool[i] < indexPool[i + 1])
+						break;
+
+				if (i == -1)
+					isFinished = true;
+				else {
+					int ceilIndex = findCeil(indexPool, indexPool[i], i + 1,
+							size - 1);
+
+					swap(indexPool, i, ceilIndex);
+
+					reverse(indexPool, i + 1, size - 1);
+				}
+			}
+		}
+
+		String s = "";
+		for (int k : shortestPath) {
+			s += (char) (k + 65);
+		}
+		System.out.println("shortest path: " + s + " with a distance of " + shortestDistance);
+
+		long endTime = System.currentTimeMillis();
+		long totalTime = endTime - startTime;
+		System.out.println("The time taken to run the program is " + totalTime / 1000.0 + " seconds");
+	}
+}
--- DisplayPanel.java
+++ DisplayPanel.java
@@ -12,32 +12,29 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class DisplayPanel extends JFrame{
-	
-	private int screenBorder, sidePanelWidth, screenWidth, screenHeight;
-	
-	private JCheckBox displayAllPossiblePath, displayShortestPath, displayCoordinates;
-	
-	GridPanel grid;
-	
-	DisplayPanel(Dimension screenDimension, int _sidePanelWidth, int border) {
-		
-		screenBorder = border;
-		sidePanelWidth = _sidePanelWidth;
-		
-		screenWidth = screenDimension.width;
-		screenHeight = screenDimension.height;
-		
-		Font titleFont = new Font("SANS_SERIF", Font.BOLD, 15);
-		Color color = Color.white;
-		
-		grid = new GridPanel(screenWidth - sidePanelWidth, screenHeight);
-		
-		
-        
-    	
+public class DisplayPanel extends JFrame {
+
+    private int screenBorder, sidePanelWidth, screenWidth, screenHeight;
+
+    private JCheckBox displayAllPossiblePath, displayShortestPath, displayCoordinates;
+
+    GridPanel grid;
+
+    DisplayPanel(Dimension screenDimension, int _sidePanelWidth, int border) {
+
+        screenBorder = border;
+        sidePanelWidth = _sidePanelWidth;
+
+        screenWidth = screenDimension.width;
+        screenHeight = screenDimension.height;
+
+        Font titleFont = new Font("SANS_SERIF", Font.BOLD, 15);
+        Color color = Color.white;
+
+        grid = new GridPanel(screenWidth - sidePanelWidth, screenHeight);
+
         this.setTitle("Shortest Path Finder");
-        
+
         this.setSize(screenWidth, screenHeight);
         this.setResizable(false);
         this.setLayout(null);
@@ -49,29 +46,27 @@ public class DisplayPanel extends JFrame{
         sidePanel.setBackground(color);
         sidePanel.setBounds(screenWidth - sidePanelWidth, 0, screenWidth, screenHeight);
         this.add(sidePanel);
-        
+
         JPanel gridJPanel = new JPanel();
         gridJPanel.setBackground(color);
         gridJPanel.setBounds(0, 0, screenWidth - sidePanelWidth, screenHeight);
         this.add(gridJPanel);
         gridJPanel.add(grid);
-        
-        
+
         JLabel size = new JLabel("Number of Nodes");
         size.setBounds(20, 10, 200, 20);
         size.setFont(titleFont);
         sidePanel.add(size);
-        
-        JTextField size_Field = new JTextField("4");
+
+        JTextField size_Field = new JTextField("6");
         size_Field.setBounds(20, 35, 50, 20);
         sidePanel.add(size_Field);
-        
-        
+
         JLabel displayOptions = new JLabel("Display Options");
         displayOptions.setBounds(20, 75, 200, 20);
         displayOptions.setFont(titleFont);
         sidePanel.add(displayOptions);
-        
+
         displayCoordinates = new JCheckBox("Display the coordinates of the nodes");
         displayCoordinates.setSelected(true);
         displayCoordinates.setBackground(color);
@@ -83,95 +78,91 @@ public class DisplayPanel extends JFrame{
         displayAllPossiblePath.setBackground(color);
         displayAllPossiblePath.setBounds(20, 125, 200, 20);
         sidePanel.add(displayAllPossiblePath);
-        
+
         displayShortestPath = new JCheckBox("Display the shortest path");
         displayShortestPath.setSelected(true);
         displayShortestPath.setBackground(color);
         displayShortestPath.setBounds(20, 150, 200, 20);
         sidePanel.add(displayShortestPath);
-        
-    
+
         JLabel controls = new JLabel("Controls");
         controls.setBounds(20, 185, 100, 20);
         controls.setFont(titleFont);
         sidePanel.add(controls);
-        
+
         JButton randomize = new JButton("Randomize");
         randomize.setBounds(20, 210, 100, 20);
         sidePanel.add(randomize);
-        
+
         JButton solve = new JButton("Solve");
         solve.setBounds(130, 210, 100, 20);
         sidePanel.add(solve);
-        
-        
+
         JLabel shortestPathTitle = new JLabel("Results");
         shortestPathTitle.setBounds(20, 255, 200, 20);
         shortestPathTitle.setFont(titleFont);
         sidePanel.add(shortestPathTitle);
- 
-		JLabel shortestPathList = new JLabel("Not yet generated");
-		shortestPathList.setBounds(20, 280, 200, 20);
-		shortestPathList.setFont(titleFont);
-		sidePanel.add(shortestPathList);
-        
-        
+
+        JLabel shortestPathList = new JLabel("Not yet generated");
+        shortestPathList.setBounds(20, 280, 200, 20);
+        shortestPathList.setFont(titleFont);
+        sidePanel.add(shortestPathList);
+
         Graph graph = new Graph();
-        
+
         resetCheckboxes();
-        
+
         displayAllPossiblePath.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
-            	System.out.println(displayAllPossiblePath.isSelected());
-            	grid.setShowAllPath(displayAllPossiblePath.isSelected());
+                System.out.println(displayAllPossiblePath.isSelected());
+                grid.setShowAllPath(displayAllPossiblePath.isSelected());
             }
         });
-        
+
         displayShortestPath.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
-        		grid.setShowShortestPath(displayShortestPath.isSelected());
+                grid.setShowShortestPath(displayShortestPath.isSelected());
             }
         });
-        
+
         displayCoordinates.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
-        		grid.setShowCoordinates(displayCoordinates.isSelected());
+                grid.setShowCoordinates(displayCoordinates.isSelected());
             }
         });
-        
+
         randomize.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
                 int numNodes = Integer.parseInt(size_Field.getText());
-                
+
                 graph.generateNodes(numNodes, screenWidth - sidePanelWidth - screenBorder, screenHeight - screenBorder);
-                
 
                 grid.clearGrid();
-                
+
                 grid.plotGraph(graph);
                 grid.drawLine(graph);
-                
-                
-                //Algorithm.permute(graph);
-                //graph.linkNodes(Algorithm.shortestPath);
 
-                //grid.drawLine(graph);
+                // Algorithm.permute(graph);
+                // graph.linkNodes(Algorithm.shortestPath);
+
+                // grid.drawLine(graph);
             }
         });
-        
+
         solve.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
-                //int numNodes = Integer.parseInt(size_Field.getText());
-                
-                //graph.generateNodes(numNodes, screenWidth - sidePanelWidth - screenBorder, screenHeight - screenBorder);
-                
-            	displayShortestPath.setSelected(true);
-        		grid.setShowShortestPath(true);
-            	
+                // int numNodes = Integer.parseInt(size_Field.getText());
+
+                // graph.generateNodes(numNodes, screenWidth - sidePanelWidth - screenBorder,
+                // screenHeight - screenBorder);
+
+                displayShortestPath.setSelected(true);
+                grid.setShowShortestPath(true);
+
                 grid.clearGrid();
-                
+
                 grid.plotGraph(graph);
-                
+
                 Algorithm.permute(graph);
                 graph.linkNodes(Algorithm.shortestPath);
 
@@ -179,15 +170,15 @@ public class DisplayPanel extends JFrame{
             }
         });
     }
-	
-	private void resetCheckboxes() {
-		displayAllPossiblePath.setSelected(true);
-		displayShortestPath.setSelected(true);
-		displayCoordinates.setSelected(true);
-		
-		grid.setShowShortestPath(true);
-		grid.setShowAllPath(true);
-		grid.setShowCoordinates(true);
-		
-	}
+
+    private void resetCheckboxes() {
+        displayAllPossiblePath.setSelected(true);
+        displayShortestPath.setSelected(true);
+        displayCoordinates.setSelected(true);
+
+        grid.setShowShortestPath(true);
+        grid.setShowAllPath(true);
+        grid.setShowCoordinates(true);
+
+    }
 }
